7df6c744411965ea4378c12a54b3fee54ea6ee8e
sand <29268036@qq.com>
2019-05-14 00:07:08 +0800

    while循环

--- 3.循环语句.py
+++ 3.循环语句.py
@@ -4,7 +4,31 @@
 # @FileName: 3.循环语句.py
 # @Project : OldBoy
 
-count = 1
-while count <= 10:
-    print(count)
-    count += 1
+# count = 1
+# while count <= 10:
+#     if count == 7:
+#         pass
+#     else:
+#         print(count)
+#     count += 1
+
+
+# count = 1
+# while count <= 10:
+#     if count != 7:
+#         print(count)
+#     count += 1
+
+# while True:           break是跳出当前循环
+#     print('你好')
+#     while True:
+#         print(666)
+#         break
+
+# count = 1               本次循环如果遇到continue，则不再继续往下走，而是回到while循环条件位置
+# while count <= 10:
+#     if count == 7:
+#         count += 1
+#         continue
+#     print(count)
+#     count = count + 1
