ddee215d365a29e3975ddd3baac5151036953cd6
number717 <29268036@qq.com>
2019-11-19 15:05:34 +0800

    校园IT管理系统更新

--- apeland/校园IT管理系统.py
+++ apeland/校园IT管理系统.py
@@ -16,54 +16,68 @@ class School(object):
         self.name = name
         self.addr = addr
         self.website = website
-        self.branches = {}
-        self.__staff_list = []
+        self.branches = []
+        self.class_list = []  # 存班级列表
+        self.staff_list = []
         self.balance = 0
         print("初始化校区【%s】,地址:%s..." % (self.name, self.addr))
 
-    def pay_roll(self, staff_list):
-        total = 0
-        for staff in staff_list:
-            total += staff.get_salary()
-        self.__school_balance -= total
-        create_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-        print(create_time + "%s发工资，账户余额:%s" % (total, self.__school_balance))
+    def pay_roll(self):
+        print("开始发11月份的工资啦。。。。")
+        for staff in self.staff_list:
+            staff.balance += staff.salary
+            self.balance -= staff.salary
+            create_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+            print(create_time + " 给%s发了%s工资，他的账户余额:%s" % (staff.name, staff.salary, staff.balance))
+        print("公司余额：", self.balance)
 
     def count_staff_num(self):
         """统计公司各分校员工人数"""
-        total_staff_num = len(self.__staff_list)  # 当前校区员工数
+        total_staff_num = len(self.staff_list)  # 当前校区员工数
         for i in self.branches:
-            total_staff_num += len(self.branches[i].__staff_list)
+            total_staff_num += len(self.branches[i].staff_list)
         print("[%s]总员工数量:%s" % (self.name, total_staff_num))
         return total_staff_num
-
-    def get_staff_num(self):
-        return len(self.__staff_list)
 
     def count_student_num(self):
         print("学员人数：", len(self.student_list))
         return len(self.student_list)
 
     def new_staff_enrollment(self, staff_obj):
-        self.__staff_list.append(staff_obj)
+        self.staff_list.append(staff_obj)
         create_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
         print(create_time + " 员工注册成功，员工姓名：【%s】，年龄：【%s】，职务：【%s】，校区：【%s】" % (
             staff_obj.name, staff_obj.age, staff_obj.position, self.name))
 
+    def count_class_list(self):
+        print("-----各校区班级-----")
+        print(self.name, self.class_list)
+        for i in self.branches:
+            print(i.name, i.class_list)
+
     def count_total_revenue(self):
-        balance = self.__school_balance
-        for branch_name, branch_obj in self.branches.items():
-            balance += branch_obj.__school_balance
-        print("Total balance:", balance)
+        print("-----校区总收入-----")
+        total_rev = self.balance
+        print(self.name, self.balance)
+        for b in self.branches:
+            print(b.name, b.balance)
+            total_rev += b.balance
+        print("各校区总收入:%s" % (total_rev,))
+
+    def __str__(self):
+        return self.name
+
+    def __repr__(self):
+        return self.name
 
 
 class BranchSchool(School):
     """分校"""
 
-    def __init__(self, name, addr, head_quater_obj):
-        super().__init__(name, addr)
-        self.head_quater = head_quater_obj
-        self.head_quater.branches[name] = self
+    def __init__(self, name, addr, website, head_quarter_obj):
+        super().__init__(name, addr, website)
+        self.head_quarter = head_quarter_obj
+        self.head_quarter.branches.append(self)  # 把自己加入到总校的校区列表李，建立总部-》分校的反向关联
 
 
 class Course(object):
@@ -84,6 +98,8 @@ class Class(object):
         self.semester = semester
         self.school_obj = school_obj
         self.stu_list = []  # 存放学员列表
+
+        school_obj.class_list.append(self)  # 把自己加到校区的班级列表
         print("校区【%s】创建了班级【%s】学期【%s】..." % (school_obj.name, course_obj.name, semester))
 
     def stu_transfer(self, stu_obj, new_class_obj):
@@ -96,6 +112,10 @@ class Class(object):
         pass
 
     def __str__(self):
+        """返回一个对象的描述信息"""
+        return "%s-%s-%s学期" % (self.school_obj.name, self.course_obj.name, self.semester)
+
+    def __repr__(self):
         """返回一个对象的描述信息"""
         return "%s-%s-%s学期" % (self.school_obj.name, self.course_obj.name, self.semester)
 
@@ -148,11 +168,13 @@ class Staff(object):
         self.dept = dept
         self.school_obj = school_obj
 
+        school_obj.new_staff_enrollment(self)  # 办入职
+
     def punch_card(self):
         pass
 
     def count_school_balance(self):
-        return self.school_obj.school_balance
+        return self.school_obj.balance
 
 
 class Teacher(Staff):
@@ -166,11 +188,11 @@ class Teacher(Staff):
 
 if __name__ == '__main__':
     # 创建校区
-    head_quater = School("崇仁路小学", "硚口区崇仁路", "master.com")
-    sh1_school = School("崇仁路小学汉西分校", "硚口区汉西路", "master.com")
-    sh2_school = School("崇仁路小学古田分校", "硚口区古田", "master.com")
-    sz1_school = School("崇仁路小学洪山分校", "洪山亚贸", "master.com")
-    sz2_school = School("崇仁路小学青山分校", "青山八大家", "master.com")
+    head_quarter = School("崇仁路小学", "硚口区崇仁路", "master.com")
+    sh1_school = BranchSchool("崇仁路小学汉西分校", "硚口区汉西路", "master.com", head_quarter)
+    sh2_school = BranchSchool("崇仁路小学古田分校", "硚口区古田", "master.com", head_quarter)
+    sz1_school = BranchSchool("崇仁路小学洪山分校", "洪山亚贸", "master.com", head_quarter)
+    sz2_school = BranchSchool("崇仁路小学青山分校", "青山八大家", "master.com", head_quarter)
 
     # 创建课程
     py_course = Course("Python", 21800, None)
@@ -179,15 +201,15 @@ if __name__ == '__main__':
     go_course = Course("Python", 22800, None)
 
     # 创建班级
-    py_24 = Class(py_course, 24, head_quater)
+    py_24 = Class(py_course, 24, head_quarter)
     py_12 = Class(py_course, 12, sh1_school)
     linux_63 = Class(linux_course, 63, sz1_school)
-    go_5 = Class(go_course, 5, head_quater)
+    go_5 = Class(go_course, 5, head_quarter)
 
     # 创建员工、老师、学员
-    s1 = Staff('Sand', 33, 0, 3000, "校长", '校长办公室', head_quater)
-    s2 = Staff('Summery', 33, 0, 3000, '主任', '政教办公室', head_quater)
-    s3 = Staff("Cathy", 29, 0, 1000, '财务', '财务办公室', head_quater)
+    s1 = Staff('Sand', 33, 0, 3000, "校长", '校长办公室', head_quarter)
+    s2 = Staff('Summery', 33, 0, 3000, '主任', '政教办公室', head_quarter)
+    s3 = Staff("Cathy", 29, 0, 1000, '财务', '财务办公室', head_quarter)
     s4 = Staff("VC", 29, 0, 2000, "校长", '总务处', sh1_school)
     s5 = Staff("二哈", 49, 0, 800, "门房", '保卫处', sz1_school)
     s6 = Staff("赵卫国", 39, 0, 1000, "厨师", '后勤', sz2_school)
@@ -205,4 +227,15 @@ if __name__ == '__main__':
     stu3 = Student("小强", 22, 35000, go_5)
     stu3 = Student("小虎", 28, 38000, linux_63)
 
-    print(head_quater.balance)
+    print(head_quarter.balance)
+    print(sz1_school.balance)
+
+    print(head_quarter.branches)
+    # 统计 账户余额、班级
+    head_quarter.count_total_revenue()
+    head_quarter.count_class_list()
+    # 发工资
+    head_quarter.pay_roll()
+    sz1_school.pay_roll()
+    sz2_school.pay_roll()
+    sh1_school.pay_roll()
